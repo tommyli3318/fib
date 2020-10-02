74a53bbad72496649fa1dc22e37f8f1a54632076
Alyssa Maquiling <alymaquiling@gmail.com>
2020-10-01 20:51:23 -0400

    added api gw, fixed lambda a

--- app-code/gsa-lambda-a.py
+++ app-code/gsa-lambda-a.py
@@ -1,23 +1,37 @@
+# Lambda A: 
+# read DB to avoid used names
+# read S3 to get first unused name, then return it to step function
+
 import json
 import boto3
-from datetime import datetime as dt
 
-# setting up client for dynamodb
-table_name = 'gsa-fibonacci-table'
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table(table_name)
-
-def lambda_handler(message, context):
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3')
+    obj = s3.Object('gsa-fibonacci-s3-bucket', 'names.txt')
+    body = obj.get()['Body'].read().decode("utf-8")
     
-    sum = message['firstVal'] + message['secondVal']
-    sequence_label = message['sequenceLabel']
+    names = body.splitlines()
     
-    response = table.put_item(
-        Item = {
-            'fib_value': sum,
-            'sequence_label': sequence_label
-        }
-    )
+    # setting up client for dynamodb
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('gsa-fibonacci-table')
     
-    return response
-
+    for name in names:
+        response = table.get_item(
+            Key={
+                'sequence_label': name
+            }
+        )
+        
+        # skip already used names
+        if not response.get("Item"):
+            return {
+                'statusCode': 200,
+                'sequenceLabel': name
+            }
+    
+    # reached the end of file, all names were used
+    return {
+        'statusCode': 200,
+        'sequenceLabel': None
+    }
--- app-def/fib-stack.ts
+++ app-def/fib-stack.ts
@@ -1,54 +1,54 @@
-import * as cdk from '@aws-cdk/core';
-import * as lambda from '@aws-cdk/aws-lambda'; // https://docs.aws.amazon.com/cdk/api/latest/docs/aws-lambda-readme.html
-import * as sfn from '@aws-cdk/aws-stepfunctions'; // https://docs.aws.amazon.com/cdk/api/latest/docs/aws-stepfunctions-readme.html
-import * as tasks from '@aws-cdk/aws-stepfunctions-tasks'; // https://docs.aws.amazon.com/cdk/api/latest/docs/aws-stepfunctions-tasks-readme.html
-import { Rule, Schedule } from '@aws-cdk/aws-events'; // https://docs.aws.amazon.com/cdk/api/latest/docs/aws-events-readme.html
-import { SfnStateMachine } from '@aws-cdk/aws-events-targets'; // https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-events-targets.SfnStateMachine.html
-
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda"; // https://docs.aws.amazon.com/cdk/api/latest/docs/aws-lambda-readme.html
+import * as sfn from "@aws-cdk/aws-stepfunctions"; // https://docs.aws.amazon.com/cdk/api/latest/docs/aws-stepfunctions-readme.html
+import * as tasks from "@aws-cdk/aws-stepfunctions-tasks"; // https://docs.aws.amazon.com/cdk/api/latest/docs/aws-stepfunctions-tasks-readme.html
+import { Rule, Schedule } from "@aws-cdk/aws-events"; // https://docs.aws.amazon.com/cdk/api/latest/docs/aws-events-readme.html
+import { SfnStateMachine } from "@aws-cdk/aws-events-targets"; // https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-events-targets.SfnStateMachine.html
+import * as apigw from "@aws-cdk/aws-apigateway";
 
 export class FibStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // define lambdas
-    const lambdaA = new lambda.Function(this, 'lambdaA', {
+    const lambdaA = new lambda.Function(this, "lambdaA", {
       runtime: lambda.Runtime.PYTHON_3_7,
-      code: lambda.Code.fromAsset('app-code'),
-      handler: 'gsa-lambda-a.lambda_handler' // file is "gsa-lambda-a", function is "lambda_handler"
+      code: lambda.Code.fromAsset("app-code"),
+      handler: "gsa-lambda-a.lambda_handler", // file is "gsa-lambda-a", function is "lambda_handler"
     });
 
-    const lambdaB = new lambda.Function(this, 'lambdaB', {
+    const lambdaB = new lambda.Function(this, "lambdaB", {
       runtime: lambda.Runtime.PYTHON_3_7,
-      code: lambda.Code.fromAsset('app-code'),
-      handler: 'gsa-lambda-b.lambda_handler'
+      code: lambda.Code.fromAsset("app-code"),
+      handler: "gsa-lambda-b.lambda_handler",
     });
 
-    const lambdaC = new lambda.Function(this, 'lambdaC', {
+    const lambdaC = new lambda.Function(this, "lambdaC", {
       runtime: lambda.Runtime.PYTHON_3_7,
-      code: lambda.Code.fromAsset('app-code'),
-      handler: 'gsa-lambda-c.lambda_handler'
+      code: lambda.Code.fromAsset("app-code"),
+      handler: "gsa-lambda-c.lambda_handler",
     });
 
-    const lambdaD = new lambda.Function(this, 'lambdaD', {
+    const lambdaD = new lambda.Function(this, "lambdaD", {
       runtime: lambda.Runtime.PYTHON_3_7,
-      code: lambda.Code.fromAsset('app-code'),
-      handler: 'gsa-lambda-d.lambda_handler'
+      code: lambda.Code.fromAsset("app-code"),
+      handler: "gsa-lambda-d.lambda_handler",
     });
-
 
     // define step function tasks
-    const taskLambdaA = new tasks.LambdaInvoke(this, 'task-invokeA', {
+    const taskLambdaA = new tasks.LambdaInvoke(this, "task-invokeA", {
       lambdaFunction: lambdaA,
       comment: "Returns last sequence value from S3",
-      outputPath: "$.outputA"
+      outputPath: "$.outputA",
     });
-    const taskLambdaB = new tasks.LambdaInvoke(this, 'task-invokeB', {
+    const taskLambdaB = new tasks.LambdaInvoke(this, "task-invokeB", {
       lambdaFunction: lambdaB,
-      comment: "Returns last sequence value with last two inserted fibonacci values",
+      comment:
+        "Returns last sequence value with last two inserted fibonacci values",
       inputPath: "$.outputA",
-      outputPath: "$.outputB"
+      outputPath: "$.outputB",
     });
-    const taskLambdaC = new tasks.LambdaInvoke(this, 'task-invokeC', {
+    const taskLambdaC = new tasks.LambdaInvoke(this, "task-invokeC", {
       lambdaFunction: lambdaC,
       comment: "Sum of two values",
       inputPath: "$.outputB",
@@ -58,16 +58,19 @@ export class FibStack extends cdk.Stack {
     const stepChain = taskLambdaA.next(taskLambdaB).next(taskLambdaC);
 
     // https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-stepfunctions.StateMachine.html
-    const stateMachine = new sfn.StateMachine(this, 'stateMachine', {
+    const stateMachine = new sfn.StateMachine(this, "stateMachine", {
       definition: stepChain,
-      stateMachineName: 'Run-Lambdas'
+      stateMachineName: "Run-Lambdas",
     });
 
     // aws-event rule, tell step function to run once a day
-    new Rule(this, 'ScheduleRule', {
-      schedule: Schedule.cron({ minute: '0', hour: '12' }),
+    new Rule(this, "ScheduleRule", {
+      schedule: Schedule.cron({ minute: "0", hour: "12" }),
       targets: [new SfnStateMachine(stateMachine)],
-     });
+    });
 
+    new apigw.LambdaRestApi(this, "Endpoint", {
+      handler: lambdaD,
+    });
   }
 }
